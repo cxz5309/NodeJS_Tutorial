b192b2662a8514aa9ec2e8f5e8a3769f9d77ab92
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-02-12 23:51:18 +0900

    Update index.js

--- Yeu.kr/로그를 분석해서 기계적 요청자를 가려내보자/index.js
+++ Yeu.kr/로그를 분석해서 기계적 요청자를 가려내보자/index.js
@@ -1,17 +1,33 @@
-    function solution(weblog) {
-        const weblogArr = weblog.split("\n");
-        var logs = [];
-        weblogArr.forEach((val, index)=>{
-        var logString = val.split(' ');
-        logs[index] = {
-            ip: logString[0],
-            date: Date.parse(val.substring(val.indexOf('[') + 1, val.indexOf(']'))),
-            url: logString[10]
-        }
-        })
-        console.log(logs[0])
-        console.log(logs[1])
-        console.log(logs[2])
-        //아직 하는 중 - 현재 전체 로그 오브젝트로 분리하는것까지 종료, 앞으로 ip, url이 동일한 로그끼리 date분석해서 타임라인의 차가 모두 같으면 리턴해야함
-        return [];
+function solution(weblog) {
+    const weblogArr = weblog.split("\n");
+    var logs = [];
+    weblogArr.forEach((val, index)=>{
+    var logString = val.split(' ');
+    logs[index] = {
+       ip: logString[0],
+       date: Date.parse(val.substring(val.indexOf('[') + 1, val.indexOf(']'))),
+       url: logString[10]
     }
+    })
+    console.log(logs[0])
+    console.log(logs[1])
+    console.log(logs[2])
+    //아직 하는 중 - 현재 전체 로그 오브젝트로 분리하는것까지 종료, 앞으로 ip, url이 동일한 로그끼리 date분석해서 타임라인의 차가 모두 같으면 리턴해야함
+    var obj = {};
+    logs.reduce((acc, curr) => {
+       if(!acc[curr.ip] || acc[curr.ip]==null){
+          acc[curr.ip] = [curr.url, curr.date];
+          console.log("없을경우:" + acc[curr.ip]);
+       }
+       else{
+          console.log("있을경우:" + acc[curr.ip]);
+          if(acc[curr.ip][0]!= curr.url){
+             acc.splice(curr.ip, 1);
+          }
+          else{
+             console.log(acc.length);
+          }
+       }
+    }, obj)
+    return [];
+ }
