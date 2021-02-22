a2e62763ffecc297a2bf8bedf84ea238dbc17cc4
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-02-22 23:11:07 +0900

    Update index.js

--- Yeu.kr/로그를 분석해서 기계적 요청자를 가려내보자/index.js
+++ Yeu.kr/로그를 분석해서 기계적 요청자를 가려내보자/index.js
@@ -1,64 +1,103 @@
+// function solution(weblog) {
+//    const weblogArr = weblog.split("\n");
+//    var logs = [];
+//    weblogArr.forEach((val, index)=>{
+//       var logString = val.split(' ');
+//       logs[index] = {
+//          ip: logString[0],
+//          date: Date.parse(val.substring(val.indexOf('[') + 1, val.indexOf(']'))),
+//          url: logString[10]
+//       }
+//    })
+//    // console.log(logs[0])
+//    // console.log(logs[1])
+//    // console.log(logs[2])
+//    //아직 하는 중 - 현재 전체 로그 오브젝트로 분리하는것까지 종료, 앞으로 ip, url이 동일한 로그끼리 date분석해서 타임라인의 차가 모두 같으면 리턴해야함
+//    console.log(logs);
+//    var timeCut = new Map;
+//    logs.forEach((val, i)=>{
+//       if(!timeCut.has(val.ip)){
+//          timeCut.set(val.ip, {
+//             nowURL:val.url,
+//             nowDate:val.date,
+//             cut:0
+//          });
+//       }
+//       else{
+//          if(timeCut.get(val.ip).nowURL !== val.url){
+//             return;
+//          }
+//          timecut = val.date - timeCut.get(val.ip).nowDate;
+//          console.log("timecut"+ timecut);
+//          if(timeCut.get(val.ip).cut!=0){
+//             if(timecut != timeCut.get(val.ip).cut){
+//                return;
+//             }
+//          }
+//          var nowVal = {
+//             nowURL:val.url,
+//             nowDate:val.date,
+//             cut:timecut
+//          }
+         
+//          timeCut.set(val.ip, nowVal)
+//       }
+//    })
+//    console.log(timeCut.size)
+//    timeCut.forEach((val,key)=>{
+//       if(val.cut == 0){
+//          timeCut.delete(key);
+//       }
+//    })
+//    console.log(timeCut.size)
+//    let keys = Array.from( timeCut.keys() );
+//    return keys;
+// }
+
+
+
+//일단 답인데 나중에 해석할것
+
 function solution(weblog) {
-   const weblogArr = weblog.split("\n");
-   var logs = [];
-   weblogArr.forEach((val, index)=>{
-   var logString = val.split(' ');
-   logs[index] = {
-      ip: logString[0],
-      date: Date.parse(val.substring(val.indexOf('[') + 1, val.indexOf(']'))),
-      url: logString[10]
-   }
+
+   const logs = weblog
+   .split('\n')
+   .map(str => str.match(/(^[^ ]+)|(\[.+(?=]))|([^"]+(?=" \d))/g)) // /(^[^ ]+)|(\[.+(?=]))|(\/.+(?=HTTP))/g
+   .reduce((acc, [ip, time, uri]) => {
+      time = +new Date(time.replace('[', ''));
+      if (acc[ip]) {
+         acc[ip].push({ time, uri });
+      } else {
+         acc[ip] = [{ time, uri }];
+      }
+
+      return acc;
+   }, {});
+
+   console.log(logs);
+
+   const result = Object
+   .entries(logs)
+   .filter(([, uris]) => uris.length > 2)
+   .filter(([, uris]) => {
+      return uris.filter(({ uri }) => uris[0].uri !== uri).length === 0;
    })
-   // console.log(logs[0])
-   // console.log(logs[1])
-   // console.log(logs[2])
-   //아직 하는 중 - 현재 전체 로그 오브젝트로 분리하는것까지 종료, 앞으로 ip, url이 동일한 로그끼리 date분석해서 타임라인의 차가 모두 같으면 리턴해야함
-   var obj = new Map;
-   console.log("conloglen : " + logs.length);
+   .filter(([, uris]) => {
+      const times = uris.map(e => e.time).sort((a, b) => a - b);
 
-   var delIP = [];
-   logs.reduce((acc, curr) => {
-      if(!delIP.includes(curr.ip)){
-         if(!obj.has(curr.ip)){
-            obj.set(curr.ip,[curr.url, curr.date, 0]);
-         // console.log("없을경우:" + obj.get(curr.ip));
-         }
-         else{
-            if(obj.get(curr.ip)[0]!= curr.url){
-               delIP.push(curr.ip);
-               obj.delete(curr.ip);
-            }
-            else{
-               const timeCut = (curr.date - obj.get(curr.ip)[1])
-
-               if(obj.get(curr.ip)[2] == 0){
-                  const tmp = obj.get(curr.ip);
-                  tmp[2] = timeCut;
-                  obj.set(curr.ip, tmp);
-               }
-               else{
-                  if(obj.get(curr.ip)[2] != timeCut){
-                     console.log(curr);
-                     console.log(" , " + timeCut);
-                     console.log(obj.get(curr.ip))
-                     delIP.push(curr.ip);
-                     obj.delete(curr.ip);
-                  }
-               }
-            }
+      const diff = times[1] - times[0];
+      for (let i = 2; i < times.length; i++) {
+         if (times[i] - times[i - 1] !== diff) {
+            return false;
          }
       }
+
+      return true;
    })
-   console.log(obj.size);
-   console.log("START");
+   .map(([ip]) => ip);
 
-   Array.from(obj.keys()).map(key => {
-      console.log(key);
-   });
-   Array.from(obj.values()).map(key => {
-      console.log(key);
-   });
-   
+   console.log(result);
 
-   return [];
+   return result;
 }
+
