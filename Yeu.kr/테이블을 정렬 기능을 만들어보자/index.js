e9d8d9f61218cb90e49d2333f4fa6417e5dc2caa
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-04-08 08:01:30 +0900

    not use this

--- Yeu.kr/테이블을 정렬 기능을 만들어보자/index.js
+++
@@ -1,5 +0,0 @@
-function solution() {
-    // 여기에 코드를 작성해주세요
-    let table = [...document.querySelectorAll('#stage')];
-    let column_list = [...document.querySelectorAll('#stage>thead>tr>th')];
- }
